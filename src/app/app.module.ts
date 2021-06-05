06512d7bebf7623bb1294c023c3183968886454b
Lalo-full <gerardomtzisc@gmail.com>
2021-06-05 14:21:57 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { NurseComponent } from './components/nurse/nurse.component';
+import { StaffComponent } from './components/staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DoctorComponent,
+    NurseComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule,
