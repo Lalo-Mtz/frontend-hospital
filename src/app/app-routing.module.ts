185ecae228973e141a1dcd5b36c0f41f1d673fb4
brayancapetillo <brayancapetoccc@gmail.com>
2021-06-06 01:01:35 -0500

    termine app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { HomeComponent } from './components/home/home.component';
+import { NurseComponent } from './components/nurse/nurse.component';
+import { StaffComponent } from './components/staff/staff.component';
+
+
+const routes: Routes = [
+  {path: "doctor",component:DoctorComponent},
+  {path: "",component:HomeComponent},
+  {path: "nurse",component:NurseComponent},
+  {path: "staff",component:StaffComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
