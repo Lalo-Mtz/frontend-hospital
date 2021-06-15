1aa6e9c1922c7e9802d0721e87db742592dd317c
Lalo-full <gerardomtzisc@gmail.com>
2021-06-14 20:15:05 -0500

    I Found sweet alert

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -20,14 +22,41 @@ export class SignupComponent implements OnInit {
   }
 
   signUp(): void {
-    this.authService.signUp(this.user)
-      .subscribe(
-        res => {
-          localStorage.setItem('token', res.token)
-          this.router.navigate(['/doctor']);
-        },
-        err => console.log(err)
-      )
+    if (this.user.username != '' && this.user.email != '' && this.user.password != '') {
+      this.authService.signUp(this.user)
+        .subscribe(
+          res => {
+            this.router.navigate(['/doctor']);
+            // Swal.fire({
+            //   position: 'top-end',
+            //   icon: 'success',
+            //   title: 'Your acount has been saved',
+            //   showConfirmButton: false,
+            //   timer: 5000
+            // })
+            Swal.fire({
+              title: "Verify your email",
+              text: "We send a confirmation to your email. Check it out",
+              showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+              },
+              hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+              }
+            });
+          },
+          err => {
+            console.log(err);
+          }
+        );
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You need to fill all the fields',
+        footer: 'Check that none are missing'
+      });
+    }
   }
 
 }
