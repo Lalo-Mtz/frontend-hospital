5b46e33dd29e786588ebb2c09e9adf0cf65424ef
brayancapetillo <brayancapetoccc@gmail.com>
2021-06-15 20:35:49 -0500

    cambios doctor

--- src/app/components/doctor/doctor.component.ts
+++ src/app/components/doctor/doctor.component.ts
@@ -6,6 +6,8 @@ import { DoctorService } from '../../services/doctor.service';
 import { AuthService }  from '../../services/auth.service';
 
 import Swal from 'sweetalert2';
+import { BehaviorSubject } from 'rxjs';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-doctor',
@@ -64,5 +66,14 @@ export class DoctorComponent implements OnInit {
         err => console.log(err)
       )
   }
+
+  totable(){
+    document.getElementById("Tablero")?.scrollIntoView({behavior:"smooth"});
+  }
+
+  todoctorme(){
+    document.getElementById("doctorme")?.scrollIntoView({behavior:"smooth"});
+  }
+
 
 }
